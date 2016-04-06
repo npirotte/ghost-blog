5382bb358c5504a057f3f26e8ecae43677b85557
root <root@vps265011.ovh.net>
2016-04-06 14:36:25 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,152 @@
+// # Ghost Configuration
+// Setup your Ghost install for various environments
+// Documentation can be found at http://docs.ghost.org/usage/configuration/
+
+var path = require('path'),
+    config;
+
+config = {
+    // ### Development **(default)**
+    development: {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        url: process.env.GHOST_URL,
+        mail: {
+            transport: 'SMTP',
+            options: {
+                host: process.env.MAIL_HOST
+            },
+            from: process.env.MAIL_FROM
+        },
+
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '0.0.0.0',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        },
+        paths: {
+            contentPath: path.join(process.env.GHOST_CONTENT, '/')
+        }
+    },
+
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: 'http://blog.npirotte.be/', process.env.GHOST_URL,
+        forceAdminSSL: true,
+        mail: {
+            transport: 'SMTP',
+            options: {
+                host: process.env.MAIL_HOST
+            },
+            from: process.env.MAIL_FROM
+        },
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '0.0.0.0',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        },
+        paths: {
+            contentPath: path.join(process.env.GHOST_CONTENT, '/')
+        },
+        logging: false
+    },
+
+    // **Developers only need to edit below here**
+
+    // ### Testing
+    // Used when developing Ghost to run tests and check the health of Ghost
+    // Uses a different port number
+    testing: {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-test.db')
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Travis
+    // Automated testing run through GitHub
+    'travis-sqlite3': {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-travis.db')
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Travis
+    // Automated testing run through GitHub
+    'travis-mysql': {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '0.0.0.0',
+                user     : 'travis',
+                password : '',
+                database : 'ghost_travis',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Travis
+    // Automated testing run through GitHub
+    'travis-pg': {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'pg',
+            connection: {
+                host     : '0.0.0.0',
+                user     : 'postgres',
+                password : '',
+                database : 'ghost_travis',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    }
+};
+
+// Export config
+module.exports = config;
